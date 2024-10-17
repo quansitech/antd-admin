d7dd33fedf72501c50e278e41ea120fd51301401
jack <958691165@qq.com>
2024-10-17 11:21:25 +0800

    防止valueEnum为空时出错

--- src/Component/Traits/HasValueEnum.php
+++ src/Component/Traits/HasValueEnum.php
@@ -12,14 +12,20 @@ trait HasValueEnum
      * @example [1 => ['text'=>'启用', 'status'=>'Success'], 0 => ['text'=>'禁用', 'status'=>'Error']]
      * 其中 status 的值有 Success, Warning, Error, Default, Processing。
      */
-    public function setValueEnum(array $valueEnum): static
+    public function setValueEnum($valueEnum): static
     {
         $this->render_data['valueEnum'] = $this->transformValueEnumToMap($valueEnum);
         return $this;
     }
 
+    /**
+     * 将 valueEnum 转为 map 结构，以便前端使用
+     * @param $valueEnum
+     * @return array|mixed
+     */
     protected function transformValueEnumToMap($valueEnum)
     {
+        if (empty($valueEnum)) return [];
         $res = [];
         if ($valueEnum == array_values($valueEnum)) {
             return $valueEnum;
