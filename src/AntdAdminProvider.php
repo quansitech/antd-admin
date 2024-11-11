7227c49c9f0f36c012d6240aab319956d76320c9
jack <958691165@qq.com>
2024-11-11 14:58:06 +0800

    移除软连接

--- src/AntdAdminProvider.php
+++ src/AntdAdminProvider.php
@@ -3,7 +3,6 @@
 namespace AntdAdmin;
 
 use AntdAdmin\Controller\AreaController;
-use AntdAdmin\Controller\UploadController;
 use Bootstrap\Provider;
 use Bootstrap\RegisterContainer;
 
@@ -12,7 +11,6 @@ class AntdAdminProvider implements Provider
     public function register()
     {
         $resources_path = realpath(WWW_DIR . '/../resources');
-        RegisterContainer::registerSymLink($resources_path . '/js/backend/Pages/Admin', __DIR__ . '/../resourses/js/Pages/Admin');
 
         RegisterContainer::registerController('Antd', 'Area', AreaController::class);
 
