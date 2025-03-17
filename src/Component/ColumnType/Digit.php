8244e60fb02b47fdce0f5f47564230160f9f34cf
jack <958691165@qq.com>
2025-03-17 10:08:03 +0800

    feat(qq958691165): 为 Digit 组件添加最小值和最大值限制
    - 在 Digit 类中添加了 $min 和 $max 属性
    - 实现了 setMin 和 setMax 方法，用于设置最小值和最大值
    - 在 render 方法中应用了最小值和最大值限制

--- src/Component/ColumnType/Digit.php
+++ src/Component/ColumnType/Digit.php
@@ -5,8 +5,33 @@ namespace AntdAdmin\Component\ColumnType;
 class Digit extends BaseColumn
 {
 
+    protected $min = null;
+    protected $max = null;
+
+    public function setMin($min)
+    {
+        $this->min = $min;
+        return $this;
+    }
+
+    public function setMax($max)
+    {
+        $this->max = $max;
+        return $this;
+    }
+
     protected function getValueType(): string
     {
         return 'digit';
+    }
+
+    public function render()
+    {
+        if (!isset($this->render_data['fieldProps'])) {
+            $this->render_data['fieldProps'] = [];
+        }
+        $this->render_data['fieldProps']['min'] = $this->render_data['fieldProps']['min'] ?? $this->min;
+        $this->render_data['fieldProps']['max'] = $this->render_data['fieldProps']['max'] ?? $this->max;
+        return parent::render();
     }
 }
