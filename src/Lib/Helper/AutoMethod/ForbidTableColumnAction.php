930311093fdadebf011e6c952fc728a92f5d722f
jack <958691165@qq.com>
2025-05-16 16:27:58 +0800

    修复antd-admin表格启用快捷操作问题

--- src/Lib/Helper/AutoMethod/ForbidTableColumnAction.php
+++ src/Lib/Helper/AutoMethod/ForbidTableColumnAction.php
@@ -15,7 +15,7 @@ class ForbidTableColumnAction extends BaseHelper
                 ->request('put', U('forbid'), ['ids' => '__id__'], null, '确认禁用？');
             $resume_button = $container->link('启用')
                 ->setShowCondition('status', '=', DBCont::FORBIDDEN_STATUS)
-                ->request('put', U('forbid'), ['ids' => '__id__'], null, '确认启用？');
+                ->request('put', U('resume'), ['ids' => '__id__'], null, '确认启用？');
 
             return [
                 $forbid_button,
