34579aeecb3cd1ec6a6392ae765e095eae592616
jack <958691165@qq.com>
2026-06-04 17:56:15 +0800

    refactor(Ueditor): 移除 Ueditor 组件并更新 ColumnsContainer 引用 feat(AutoCallAddComponent): 添加类型注册和获取已注册类型映射的方法

--- src/Lib/AutoCallAddComponent.php
+++ src/Lib/AutoCallAddComponent.php
@@ -8,7 +8,13 @@ trait AutoCallAddComponent
 
     abstract protected function getNamespace(): string|array;
 
-    protected static $_helper_methods = [];
+    /**
+     * 已注册的类型映射（容器类可覆盖此属性）
+     * @var array ['ueditor' => 'FormItem\\Ueditor\\Column\\Ueditor']
+     */
+    protected static array $registeredTypes = [];
+
+    protected static array $_helper_methods = [];
 
     public function __call(string $name, array $arguments)
     {
@@ -16,6 +22,13 @@ trait AutoCallAddComponent
             return call_user_func_array(self::$_helper_methods[$name], [$this, ...$arguments]);
         }
 
+        // 优先查找动态注册的类型
+        if (isset(static::$registeredTypes[$name])) {
+            $className = static::$registeredTypes[$name];
+            return call_user_func([$this, $this->getCallMethod()], new $className(...$arguments));
+        }
+
+        // 兜底：原有命名空间查找逻辑
         $class = $this->resoleClass($name);
         return call_user_func([$this, $this->getCallMethod()], new $class(...$arguments));
     }
@@ -32,11 +45,27 @@ trait AutoCallAddComponent
                 return $class;
             }
         }
-        throw new \Exception('column type: ' . $name . ' error');
+        throw new \Exception('type: ' . $name . ' not found');
     }
 
-    public static function registerHelperMethod($methodName, $callback)
+    public static function registerHelperMethod(string $methodName, callable $callback): void
     {
         self::$_helper_methods[$methodName] = $callback;
+    }
+
+    /**
+     * 注册类型（通用方法）
+     */
+    public static function registerType(string $methodName, string $className): void
+    {
+        static::$registeredTypes[$methodName] = $className;
+    }
+
+    /**
+     * 获取已注册的类型映射
+     */
+    public static function getRegisteredTypes(): array
+    {
+        return static::$registeredTypes;
     }
 }
